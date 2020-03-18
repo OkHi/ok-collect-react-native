90616fbaf5813cff06a63d7fa12187084f432992
Julius Kiano <jgkiano@gmail.com>
2020-03-18 13:53:54 +0300

    refactored app to use stack navigation

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './screens/home/Home';
+import InitScreen from './screens/init/Init';
+
+const Stack = createStackNavigator();
 
 export default function() {
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="init"
+          component={InitScreen}
+          options={{
+            header: () => {
+              return null;
+            },
+          }}
+        />
+        <Stack.Screen name="home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
