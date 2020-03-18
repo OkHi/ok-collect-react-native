5cd796bd5430fb3947fb55ca4031bf3cf5dfb547
Julius Kiano <jgkiano@gmail.com>
2020-03-18 18:57:03 +0300

    created asyncstorage repo

---
+++ example/src/services/repository.ts
@@ -0,0 +1,39 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+export default class Repository {
+  async getUser(): Promise<null | {
+    firstName: string;
+    lastName: string;
+    phone: string;
+  }> {
+    try {
+      const user = await AsyncStorage.getItem('user');
+      if (!user) {
+        return null;
+      }
+      return JSON.parse(user);
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async setUser(user: {firstName: string; lastName: string; phone: string}) {
+    try {
+      return await AsyncStorage.setItem('user', JSON.stringify(user));
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async getAddresses() {
+    try {
+      const addresses = await AsyncStorage.getItem('addresses');
+      if (!addresses) {
+        return null;
+      }
+      return JSON.parse(addresses);
+    } catch (error) {
+      return null;
+    }
+  }
+}
