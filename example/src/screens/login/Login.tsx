1c71019956b0bc9c785337874af471fd3ab488f3
Julius Kiano <jgkiano@gmail.com>
2020-03-18 19:32:49 +0300

    basic text

--- example/src/screens/login/Login.tsx
+++ example/src/screens/login/Login.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Button, Text, Container, H3} from 'native-base';
+import {View} from 'react-native';
+import styled from 'styled-components/native';
 
 export default function Login() {
-  return <Text>Login priss</Text>;
+  return (
+    <Container>
+      <LoginContainer>
+        <H3>Please log in to continue</H3>
+      </LoginContainer>
+    </Container>
+  );
 }
+
+const LoginContainer = styled.View`
+  padding: 15px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  margin: 15px;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+`;
