209b71ab3ebcd13e398e0a00ee7ac083407ebfd9
Julius Kiano <jgkiano@gmail.com>
2020-03-19 11:56:41 +0300

    created scafold AddAddress screen

---
+++ example/src/screens/add-address/AddAddress.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+
+export default class AddAddress extends React.Component {
+  constructor(props: any) {
+    super(props);
+  }
+  render() {
+    return <View />;
+  }
+}
