ef18c3404e19e40db11f177213803c0611172b3e
Julius Kiano <jgkiano@gmail.com>
2020-03-20 02:43:43 +0300

    export interface OkHiLocation

---
+++ example/src/lib/okcollect-online/OkHiLocation.ts
@@ -0,0 +1,12 @@
+export interface OkHiLocation {
+  id?: string;
+  placeId?: string;
+  plusCode?: string;
+  propertyName?: string;
+  streetName?: string;
+  title?: string;
+  subtitle?: string;
+  directions?: string;
+  otherInformation?: string;
+  url?: string;
+}
