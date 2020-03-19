744fe5370d5055b363a94af9eb4425a388d82ab3
Julius Kiano <jgkiano@gmail.com>
2020-03-19 12:05:34 +0300

    added prop defs

--- example/src/screens/home/Home.tsx
+++ example/src/screens/home/Home.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import {Container, Fab, Icon} from 'native-base';
+import {NavigationProp} from '@react-navigation/native';
 
-export default class HomeScreen extends React.Component {
-  constructor(props) {
+export default class HomeScreen extends React.Component<{
+  navigation: NavigationProp<any>;
+}> {
+  constructor(props: any) {
     super(props);
   }
+
+  handleFabPress = () => {
+    this.props.navigation.navigate('Add address');
+  };
 
   render() {
     return (
       <Container>
-        <Fab style={{backgroundColor: '#21838F'}}>
+        <Fab style={{backgroundColor: '#21838F'}} onPress={this.handleFabPress}>
           <Icon name="add" fontSize={32} />
         </Fab>
       </Container>
