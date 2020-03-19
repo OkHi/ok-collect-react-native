f46b4d06d0217b89cf91324e02d47b03eeb9982f
Julius Kiano <jgkiano@gmail.com>
2020-03-20 02:44:39 +0300

    export new modules

--- example/src/lib/okcollect-online/index.ts
+++ example/src/lib/okcollect-online/index.ts
@@ -1,2 +1,6 @@
 export * from './OkHiLocationManager';
 export * from './OkHiUser';
+export * from './OkHiAuth';
+export * from './OkHiConfig';
+export * from './OkHiLocation';
+export * from './OkHiError';
