b058d13ec2b3bc67fa05a88d2ac07836511351fc
Julius Kiano <jgkiano@gmail.com>
2020-03-20 03:18:16 +0300

    request for permissions before proceeding

--- example/src/screens/home/Home.tsx
+++ example/src/screens/home/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Platform} from 'react-native';
 import {Container, Fab, Icon} from 'native-base';
 import {NavigationProp} from '@react-navigation/native';
+import {request, PERMISSIONS} from 'react-native-permissions';
 
 export default class HomeScreen extends React.Component<{
   navigation: NavigationProp<any>;
@@ -9,8 +11,29 @@ export default class HomeScreen extends React.Component<{
     super(props);
   }
 
-  handleFabPress = () => {
+  handleFabPress = async () => {
+    await this.requestLocationPermission();
     this.props.navigation.navigate('Add address');
+  };
+
+  requestLocationPermission = async () => {
+    const rational = {
+      title: 'OkHi VaaS needs location permission',
+      message:
+        'We need location permission to enable us to create and verify the addresses you create',
+      buttonPositive: 'GRANT',
+      buttonNegative: 'DENY',
+      buttonNeutral: 'CANCEL',
+    };
+    if (Platform.OS === 'android') {
+      await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION, rational);
+      await request(PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION, rational);
+    } else if (Platform.OS === 'ios') {
+      await request(PERMISSIONS.IOS.LOCATION_ALWAYS, rational);
+      await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE, rational);
+    } else {
+      return false;
+    }
   };
 
   render() {
