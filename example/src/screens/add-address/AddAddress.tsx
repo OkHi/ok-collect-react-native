92dfaca1d1ff1207665f2ac3a37a1660ba77dff0
Julius Kiano <jgkiano@gmail.com>
2020-03-20 09:09:05 +0300

    implement OkHiLocationManager

--- example/src/screens/add-address/AddAddress.tsx
+++ example/src/screens/add-address/AddAddress.tsx
@@ -1,15 +1,66 @@
 import React from 'react';
-import {OkHiLocationManager} from '../../lib/okcollect-online';
+import {NavigationProp} from '@react-navigation/native';
+import {Toast} from 'native-base';
+import {
+  View,
+  ActivityIndicator,
+  Text,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
+import {
+  OkHiLocationManager,
+  OkHiLocation,
+  OkHiUser,
+} from '../../lib/okcollect-online';
+import {Store, User} from '../../interfaces';
 
-export default class AddAddress extends React.Component {
+function Loader() {
+  const containerStyles: StyleProp<ViewStyle> = {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+  const textStyles = {fontSize: 16, marginTop: 15};
+  return (
+    <View style={containerStyles}>
+      <ActivityIndicator color="#21838f" />
+      <Text style={textStyles}>Securing your connection with OkHi</Text>
+    </View>
+  );
+}
+
+export default class AddAddress extends React.Component<{
+  navigation: NavigationProp<any>;
+  store: Store;
+}> {
+  private user: User;
+
   constructor(props: any) {
     super(props);
+    this.user = this.props.store.user as User;
   }
+
+  handleSuccess = async (location: OkHiLocation, user: OkHiUser) => {
+    await this.props.store.setUser(user);
+    await this.props.store.setAddress(location);
+    this.props.navigation.goBack();
+  };
+
+  handleError = () => {
+    Toast.show({text: 'Opps, something went wrong. Please try again'});
+    this.props.navigation.goBack();
+  };
+
   render() {
     return (
       <OkHiLocationManager
         auth="SWF0ejlENkFOVDphZjNkZGQxMi00ZTI5LTQ1MDItODQyMS1iZTlkNmUzODcwZTU="
-        user={{phone: '+254700110590'}}
+        user={this.user}
+        style={{base: {color: '#37474F'}}}
+        loader={<Loader />}
+        onSuccess={this.handleSuccess}
+        onError={this.handleError}
       />
     );
   }
