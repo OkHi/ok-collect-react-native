cf9b422731d3faa29597f36d7efab8c0b30ee243
Julius Kiano <jgkiano@gmail.com>
2020-03-21 15:23:21 +0300

    included props for webview and safeAreaViewProps

--- example/src/lib/okcollect-online/OkHiLocationManagerProps.ts
+++ example/src/lib/okcollect-online/OkHiLocationManagerProps.ts
@@ -1,13 +1,17 @@
 import {OkHiUser, OkHiConfig, OkHiStyle, OkHiLocation, OkHiError} from './';
+import {StyleProp, ViewStyle, ViewProps} from 'react-native';
+import {WebViewProps} from 'react-native-webview';
 
 export interface OkHiLocationManagerProps {
   auth: string;
   user: OkHiUser;
   config?: OkHiConfig;
   style?: OkHiStyle;
-  onSuccess?: (location: OkHiLocation, user: OkHiUser) => any;
-  onError?: (error: OkHiError) => any;
   loader?: JSX.Element;
   launch?: boolean;
+  safeAreaViewProps?: ViewProps;
+  webviewProps?: WebViewProps;
+  onSuccess?: (location: OkHiLocation, user: OkHiUser) => any;
+  onError?: (error: OkHiError) => any;
   onCloseRequest?: () => any;
 }
