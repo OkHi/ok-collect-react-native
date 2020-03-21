8007f68ff86d4bc673539f0aa6ccf3afa2af4b82
Julius Kiano <jgkiano@gmail.com>
2020-03-21 11:04:33 +0300

    defined OkHiLocationManagerProps

---
+++ example/src/lib/okcollect-online/OkHiLocationManagerProps.ts
@@ -0,0 +1,11 @@
+import {OkHiUser, OkHiConfig, OkHiStyle, OkHiLocation, OkHiError} from './';
+
+export interface OkHiLocationManagerProps {
+  auth: string;
+  user: OkHiUser;
+  config?: OkHiConfig;
+  style?: OkHiStyle;
+  onSuccess?: (location: OkHiLocation, user: OkHiUser) => any;
+  onError?: (error: OkHiError) => any;
+  loader?: JSX.Element;
+}
