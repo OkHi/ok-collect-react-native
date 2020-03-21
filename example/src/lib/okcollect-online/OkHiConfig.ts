c535cd1955b8ddafa38f79ba808b91b304e19ee1
Julius Kiano <jgkiano@gmail.com>
2020-03-21 14:03:35 +0300

    added logo to appBar config

--- example/src/lib/okcollect-online/OkHiConfig.ts
+++ example/src/lib/okcollect-online/OkHiConfig.ts
@@ -1,8 +1,10 @@
 export interface OkHiAppBarConfiguration {
   color?: string;
   visible?: boolean;
+  logo?: string;
 }
 
 export interface OkHiConfig {
   streetView?: boolean;
+  appBar?: OkHiAppBarConfiguration;
 }
