ada4e5b0608ae9edf7effdb5e61ecd4990f844be
Julius Kiano <jgkiano@gmail.com>
2020-03-23 20:38:55 +0300

    moved modules to indiviual package

--- example/src/lib/okcollect-online/OkHiStyle.ts
+++
@@ -1,7 +0,0 @@
-export interface OkHiStyle {
-  base?: {
-    color?: string;
-    name?: string;
-    [key: string]: any;
-  };
-}
--- example/src/lib/okcollect-online/OkHiUser.ts
+++
@@ -1,6 +0,0 @@
-export interface OkHiUser {
-  id?: string;
-  firstName?: string;
-  lastName?: string;
-  phone: string;
-}
