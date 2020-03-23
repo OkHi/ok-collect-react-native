07409c5fb5a4880e2e4724f9ec0ec748c7da514d
Julius Kiano <jgkiano@gmail.com>
2020-03-23 19:01:03 +0300

    removed interfaces

--- src/index.ts
+++ src/index.ts
@@ -10,30 +10,6 @@ import { ViewProps } from 'react-native';
 import { WebViewProps } from 'react-native-webview';
 import { OkHiLocationManager } from './OkHiLocationManager';
 
-export interface OkHiStyle {
-  base?: {
-    color?: string;
-    name?: string;
-    [key: string]: any;
-  };
-}
-
-export interface OkHiLocationManagerStartPayload {
-  message: 'select_location' | 'start_app';
-  payload: {
-    user?: OkHiUser;
-    auth?: {
-      authToken: string;
-    };
-    style?: OkHiStyle;
-    context?: any;
-    config?: {
-      streetView: boolean;
-      [key: string]: any;
-    };
-  };
-}
-
 export interface OkHiLocationManagerProps {
   auth: string;
   appContext: OkHiAppContext;
