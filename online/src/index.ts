805dda3d5abb1381461e8edd5b77e926ddbd57d5
Julius Kiano <jgkiano@gmail.com>
2020-04-12 17:10:22 +0300

    defined lib specific types, removed exports from lib

--- online/src/index.ts
+++ online/src/index.ts
@@ -1,14 +1,36 @@
-import {
-  OkHiUser,
-  OkHiConfig,
-  OkHiLocation,
-  OkHiError,
-  OkHiTheme,
-  OkHiAppContext,
-} from '@okhi/types';
+import { OkHiUser, OkHiLocation, OkHiAppContext } from '@okhi/types';
 import { ViewProps } from 'react-native';
 import { WebViewProps } from 'react-native-webview';
 import { OkHiLocationManager } from './OkHiLocationManager';
+
+export interface OkHiAppBarConfiguration {
+  visible?: boolean;
+}
+
+export interface OkHiConfig {
+  streetView?: boolean;
+  appBar?: OkHiAppBarConfiguration;
+}
+
+export interface OkHiError {
+  code:
+    | 'invalid_auth_token'
+    | 'fatal_exit'
+    | 'invalid_response'
+    | 'network_request_failed'
+    | 'invalid_phone';
+  message: string;
+}
+
+export interface OkHiTheme {
+  colors?: {
+    primary: string;
+  };
+  appBar?: {
+    backgroundColor?: string;
+    logo?: string;
+  };
+}
 
 export interface OkHiLocationManagerProps {
   auth: string;
@@ -26,5 +48,3 @@ export interface OkHiLocationManagerProps {
 }
 
 export default OkHiLocationManager;
-
-export * from '@okhi/types';
