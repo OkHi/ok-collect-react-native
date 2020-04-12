cec22a6bfac20833cb6a95b23add4c741208fb99
Julius Kiano <jgkiano@gmail.com>
2020-04-12 21:10:32 +0300

    use init function from  core

--- example/index.js
+++ example/index.js
@@ -5,5 +5,13 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import OkHi, {OkHiMode, OkHiPlatformType} from '@okhi/core';
+
+OkHi.init('SWF0ejlENkFOVDphZjNkZGQxMi00ZTI5LTQ1MDItODQyMS1iZTlkNmUzODcwZTU=', {
+  app: {name: 'OkCollect Online Demo App', version: '1.0.0'},
+  platform: {name: OkHiPlatformType.HYBRID},
+  mode: OkHiMode.DEV,
+  developer: 'okhi',
+});
 
 AppRegistry.registerComponent(appName, () => App);
