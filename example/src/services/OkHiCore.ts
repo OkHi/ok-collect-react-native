21fc25cc7cff9a0fd8a91e683cde0ab32b1e05b9
Julius Kiano <jgkiano@gmail.com>
2020-04-15 18:45:20 +0300

    added app information

--- example/src/services/OkHiCore.ts
+++ example/src/services/OkHiCore.ts
@@ -4,5 +4,9 @@ export default new OkHiCore({
   auth: 'SWF0ejlENkFOVDphZjNkZGQxMi00ZTI5LTQ1MDItODQyMS1iZTlkNmUzODcwZTU=',
   context: {
     mode: OkHiMode.DEV,
+    app: {
+      name: 'OkHi Demo App',
+      version: '1.0.0',
+    },
   },
 });
